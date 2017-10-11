c29ac62bb49900cff646cb3a80746df5bd678d04
Joel Lim <103879u@gmail.com>
2017-10-11 17:39:44 +0800

    lodash and file extension

--- index.js
+++ index.js
@@ -4,17 +4,19 @@
     var walk = require('walk'),
         fs = require('fs'),
         hashFile = require('hash-file'),
-        lodash = require('lodash')    
+        lodash = require('lodash'),
+        fileExtension = require('file-extension')
+
     var dir = process.argv[2]
     var walker = walk.walk(dir)
 
-
+    var extensions = ['jpg', 'jpeg', 'png', 'mov', 'mp3', 'mp4']
     var files = {}
     var allPromises = []
     walker.on("file", function (root, fileStats, next) {
       var filePath = root + '/' + fileStats.name
       fs.readFile(filePath, function () {
-        if(fileStats.type == 'file'){
+        if(fileStats.type == 'file' && extensions.includes(fileExtension(filePath))){
           allPromises.push(hashFile(filePath).then(hash => {
             files[hash] = files[hash] || []
             files[hash].push(filePath)
