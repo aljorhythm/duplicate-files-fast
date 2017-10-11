cc720b3a67b1566dbf157ad4319cc05648916340
Joel Lim <103879u@gmail.com>
2017-10-12 01:34:19 +0800

    implement some features and fix bugs

--- duplicate-files.js
+++ duplicate-files.js
@@ -13,9 +13,21 @@ var DuplicateFiles = function () {
 DuplicateFiles.prototype = {
   findDuplicates: function (directory, options) {
     return new Promise((resolve, reject) => {
+      if (!fs.existsSync(directory)) {
+        reject({
+          error: 'INVALID_DIRECTORY'
+        })
+      }
 
       options = options || {}
-      var extensions = options['extensions'] || []
+      var extensions = options.extensions
+      var eventHandlers = options.eventHandlers || {}
+
+      function handleEvent(event, data) {
+        if (event in eventHandlers) {
+          eventHandlers[event](data)
+        }
+      }
 
       var mappedFiles = {}
       var allPromises = []
@@ -23,11 +35,17 @@ DuplicateFiles.prototype = {
       var walker = walk.walk(directory)
       walker.on("file", function (root, fileStats, next) {
         var filePath = root + '/' + fileStats.name
+        handleEvent('walked_file', {
+          file: fileStats
+        })
         fs.readFile(filePath, function () {
           if (!extensions || extensions.includes(fileExtension(filePath))) {
             allPromises.push(hashFile(filePath).then(hash => {
               mappedFiles[hash] = mappedFiles[hash] || []
               mappedFiles[hash].push(filePath)
+              handleEvent('mapped_file', {
+                file: fileStats
+              })
             }))
           }
           next()
--- index.js
+++ index.js
@@ -1,15 +1,51 @@
 var DuplicateFiles = require('./duplicate-files.js')
+var program = require('commander')
+
+function list(val) {
+  return val.split(',')
+}
+
+program
+  .version('0.1.0')
+  .option('-d, --directory <directory>', 'Root directory')
+  .option('-e, --extensions [extensions]', 'Extensions to map', list)
+  .parse(process.argv)
 
 var finder = new DuplicateFiles()
 
-var directory = process.argv[2]
-
-var options = {
-  extensions : ['jpg', 'jpeg', 'png', 'mov', 'mp3', 'mp4']
+var directory = program.directory
+if (!directory) {
+  console.log('--directory required')
+  process.exit()
 }
 
-finder.findDuplicates(directory, options).then((duplicates)=>{
-  duplicates.forEach(function(duplicate) {
+var mappedFilesCount = 0,
+  walkedFilesCount = 0
+
+function updateUiCount(msg) {
+  process.stdout.clearLine()
+  process.stdout.cursorTo(0)
+  process.stdout.write('walked\t' + walkedFilesCount + ' and mapped\t' + mappedFilesCount + ' files.\t' + (msg || ''))
+}
+
+var options = {
+  extensions: program.extensions || null,
+  eventHandlers: {
+    'walked_file': (data) => {
+      walkedFilesCount += 1
+      updateUiCount(' Walked: ' + data.file.name)
+    },
+    'mapped_file': (data) => {
+      mappedFilesCount += 1
+      updateUiCount(' Mapped: ' + data.file.name)
+    }
+  }
+}
+
+finder.findDuplicates(directory, options).then((duplicates) => {
+  duplicates.forEach(function (duplicate) {
     console.log(duplicate)
   }, this)
+}, (err) => {
+  console.log('Error: ', err.error)
 })
