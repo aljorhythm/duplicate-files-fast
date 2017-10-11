ee37a915dab27279d995cfd1ec4d2ebe0b1f2c7c
Joel Lim <103879u@gmail.com>
2017-10-11 17:59:26 +0800

    fix bug

--- index.js
+++ index.js
@@ -4,22 +4,33 @@
     var walk = require('walk'),
         fs = require('fs'),
         hashFile = require('hash-file'),
-        lodash = require('lodash'),
+        _ = require('lodash'),
         fileExtension = require('file-extension')
 
     var dir = process.argv[2]
     var walker = walk.walk(dir)
 
     var extensions = ['jpg', 'jpeg', 'png', 'mov', 'mp3', 'mp4']
-    var files = {}
+    var mappedFiles = {}
     var allPromises = []
+
+    var walkedFilesCount = 0, mappedFilesCount = 0
+
     walker.on("file", function (root, fileStats, next) {
       var filePath = root + '/' + fileStats.name
       fs.readFile(filePath, function () {
+        walkedFilesCount += 1
         if(fileStats.type == 'file' && extensions.includes(fileExtension(filePath))){
+          
           allPromises.push(hashFile(filePath).then(hash => {
-            files[hash] = files[hash] || []
-            files[hash].push(filePath)
+            mappedFiles[hash] = mappedFiles[hash] || []
+            mappedFiles[hash].push(filePath)
+
+            mappedFilesCount += 1
+
+            process.stdout.clearLine()
+            process.stdout.cursorTo(0)
+            process.stdout.write('Walked files: ' + walkedFilesCount + ' mappedFiles: ' + mappedFilesCount)
           }))
         }
         next()
@@ -32,7 +43,11 @@
 
     walker.on("end", function () {
       Promise.all(allPromises).then(function(){
-        console.log("all done")        
+          process.stdout.write('\n')
+          var filtered = _.filter(mappedFiles, function(o) { 
+            return o.length > 1
+          })
+          console.log(filtered)
       })
     })
 }())
