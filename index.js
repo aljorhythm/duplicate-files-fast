7fd8c4ada84b3a941f0462c4ca30572d9da80c14
Joel Lim <103879u@gmail.com>
2018-05-26 16:47:03 +0800

    add remove mode

--- index.js
+++ index.js
@@ -2,27 +2,32 @@
 
 let DuplicateFiles = require('./duplicate-files.js')
 let program = require('commander')
+let _ = require('lodash')
+let fs = require('fs-extra')
 
 async function main(){
   function list(val) {
-    return val.split(',')
+    return val ? val.split(',') : []
   }
 
   program
     .version('0.1.0')
     .option('-d, --directory <directory>', 'Root directory')
     .option('-e, --extensions [extensions]', 'Extensions to map', list)
-    .option('-r, --remove [remove]', 'Remove mode')
+    .option('-r, --remove', 'Remove mode')
+    .option('-x, --dryrun', 'Dry run')
     .parse(process.argv)
   
   let removeMode = program.remove
-  let finder = new DuplicateFiles()
-
+  let extensions = program.extensions
+  let dryRun = program.dryrun
   let directory = program.directory
   if (!directory) {
     console.log('--directory required')
     return
   }
+
+  let finder = new DuplicateFiles()  
 
   let mappedFilesCount = 0,
     walkedFilesCount = 0
@@ -34,7 +39,7 @@ async function main(){
   }
 
   let options = {
-    extensions: program.extensions || null,
+    extensions: extensions || null,
     eventHandlers: {
       'walked_file': (fileStats) => {
         let { name } = fileStats
@@ -49,15 +54,28 @@ async function main(){
     }
   }
   
+  let duplicates = []
   try {
-    let duplicates = await finder.findDuplicates(directory, options)
-    console.log(JSON.stringify(duplicates, 0, 4))
+    duplicates = await finder.findDuplicates(directory, options)
   } catch(err) {
     console.log('Error: ', err.error)
+    return
   }
-
+  
   if(removeMode) {
+    let toRemoveFileFromEachHash = duplicates.map((files) => files.slice(1))
+    let toRemoveFiles = _.flatten(toRemoveFileFromEachHash)
 
+    if(dryRun) {
+      console.log(toRemoveFiles.join("\n"))
+    } else {
+      let removeOperations = toRemoveFiles.map((filepath) => {
+          return fs.unlink(filepath)
+      })
+      await Promise.all(removeOperations)
+    }
+  } else {
+    console.log(JSON.stringify(duplicates, 0, 4))    
   }
 }
 
